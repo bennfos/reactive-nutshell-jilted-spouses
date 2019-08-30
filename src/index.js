b41b34bc2d34ce6f355b4eb0af25dbe3cf3f7f7b
curtis <crutchc@yahoo.com>
2019-08-30 09:23:49 -0500

    Added reactstrap and bootstrap

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter as Router } from "react-router-dom"
 import Nutshell from './components/Nutshell'
 import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 ReactDOM.render(
   <Router>
