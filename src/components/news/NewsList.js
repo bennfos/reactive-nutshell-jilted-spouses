c67706000d9aa05085394645cd9279d6bba89829
Bennett Foster <bennfos@gmail.com>
2019-09-05 09:58:07 -0500

    added functionality for seeing friends' news items dynamically

--- src/components/news/NewsList.js
+++ src/components/news/NewsList.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import NewsCard from './NewsCard'
 import NewsDataManager from './NewsDataManager'
 import NewsItemNewModal from './NewsItemNewModal'
+import NewsFriend from './NewsFriend'
+import FriendDataManager from '../friends/FriendDataManager'
 
 
 class NewsList extends Component {
@@ -9,42 +11,78 @@ class NewsList extends Component {
 //Defines initial state
     state = {
         news: [],
-        loggedInUserId: parseInt(sessionStorage.getItem("credentials"))
+        userId: parseInt(sessionStorage.getItem("credentials")),
+        connections: []
     }
 
 //When component mounts, gets all news and sets state of news array with all existsing news items
     componentDidMount(){
-        NewsDataManager.getAllNews(this.state.loggedInUserId)
+        NewsDataManager.getAllNews(this.state.userId)
         .then((news) => {
+
+            FriendDataManager.getConnections().then(connections => {
+
+                const userConnections = connections.filter(connection => {
+                  if (this.state.userId === connection.userId ||
+                    this.state.userId === connection.friendId) {
+                    return connection
+                  }
+                })
+
             this.setState({
+                connections: userConnections,
                 news: news
             })
         })
-    }
+    })
+}
 
 // Called in NewsItemNewModal (child component) to post a new object to database and update state
     addNewsItem = (newsObject) => {
     return NewsDataManager.postNewsItem(newsObject)
     .then(() => {
-        NewsDataManager.getAllNews(this.state.loggedInUserId)
+        NewsDataManager.getAllNews(this.state.userId)
         .then((news) => {
+
+            FriendDataManager.getConnections().then(connections => {
+
+                const userConnections = connections.filter(connection => {
+                  if (this.state.userId === connection.userId ||
+                    this.state.userId === connection.friendId) {
+                    return connection
+                  }
+                })
+
             this.setState({
+                connections: userConnections,
                 news: news
             })
+            })
         })
-    })
-    }
+    })}
+
 
 // Called in NewsCard(child component) to delete object from database and update state
     deleteNewsItem = (id) => {
         NewsDataManager.deleteNewsItem(id)
         .then(() => {
-        NewsDataManager.getAllNews(this.state.loggedInUserId)
+        NewsDataManager.getAllNews(this.state.userId)
         .then((news) => {
-            this.setState({
-                news: news
+
+            FriendDataManager.getConnections().then(connections => {
+
+                const userConnections = connections.filter(connection => {
+                  if (this.state.userId === connection.userId ||
+                    this.state.userId === connection.friendId) {
+                    return connection
+                  }
+                })
+
+                this.setState({
+                    connections: userConnections,
+                    news: news })
             })
-        })
+            })
         })
     }
 
@@ -52,15 +90,24 @@ class NewsList extends Component {
     postEditedNewsItem = (id) => {
         return NewsDataManager.editNewsItem(id)
         .then(() => {
-        NewsDataManager.getAllNews(this.state.loggedInUserId)
+        NewsDataManager.getAllNews(this.state.userId)
         .then((news) => {
-            this.setState({
-                news: news,
+            FriendDataManager.getConnections().then(connections => {
+
+                const userConnections = connections.filter(connection => {
+                  if (this.state.userId === connection.userId ||
+                    this.state.userId === connection.friendId) {
+                    return connection
+                  }
+                })
+
+                this.setState({
+                    connections: userConnections,
+                    news: news })
+              })
             })
         })
-        })
-    }
-
+      }
 
 
     render(){
@@ -80,10 +127,18 @@ class NewsList extends Component {
                 {...this.props}
             />
             )}
+            {this.state.connections.map(connection =>
+            <NewsFriend
+            key={connection.id}
+            connection={connection}
+            {...this.props}
+          />
+        )}
         </div>
         </React.Fragment>
-        )
-    }
+    )}
+
+
 }
 
 export default NewsList
